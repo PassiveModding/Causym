36ff218cba50bc7b2d55e35d65e5dd953359d246
Passive <passivemodding@gmail.com>
2020-01-22 22:06:04 +1100

    Add ping command

--- Modules/General/General.cs
+++ Modules/General/General.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Disqord;
 using Disqord.Bot;
 using Qmmands;
@@ -11,6 +12,18 @@ namespace Causym.Modules.General
         public async Task AvatarAsync(CachedUser user)
         {
             await ReplyAsync("", false, new LocalEmbedBuilder().WithAuthor(user).WithImageUrl(user.GetAvatarUrl()).Build());
+        }
+
+        [Command("Ping", "Latency")]
+        public async Task PingAsync()
+        {
+            var latency = Context.Bot.Latency;
+            var s = Stopwatch.StartNew();
+            var m = await ReplyAsync($"heartbeat: {latency}ms, init: ---, rtt: ---");
+            var init = s.ElapsedMilliseconds;
+            await m.ModifyAsync(x => x.Content = $"heartbeat: {latency}ms, init: {init}ms, rtt: Calculating");
+            s.Stop();
+            await m.ModifyAsync(x => x.Content = $"heartbeat: {latency}ms, init: {init}ms, rtt: {s.ElapsedMilliseconds}ms");
         }
     }
 }
