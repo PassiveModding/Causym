91c933f096dc58c366b16f58f46da9003aa7cdc7
Passive <passivemodding@gmail.com>
2020-01-22 21:49:32 +1100

    General module added

---
+++ Modules/General/General.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Qmmands;
+
+namespace Causym.Modules.General
+{
+    public class General : DiscordModuleBase
+    {
+        [Command("Avatar")]
+        public async Task AvatarAsync(CachedUser user)
+        {
+            await ReplyAsync("", false, new LocalEmbedBuilder().WithAuthor(user).WithImageUrl(user.GetAvatarUrl()).Build());
+        }
+    }
+}
