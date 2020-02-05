be36b6b736309016c2782016c2d5cf5bdd56190f
PassiveModding <passivemodding@gmail.com>
2020-02-05 22:05:50 +1100

    fix message counts growing rather than showing count since last snapshot

--- Modules/Statistics/Services/SnapshotService.cs
+++ Modules/Statistics/Services/SnapshotService.cs
@@ -96,6 +96,8 @@ namespace Causym.Modules.Statistics
 
                             db.StatSnapshots.Add(snapshot);
                         }
+
+                        GuildMessageTracker.Clear();
                     }
 
                     await db.SaveChangesAsync();
